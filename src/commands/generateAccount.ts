c88bf239dd1fbf1e3fcf27b9a899d469c43a85d5
ValentineCodes <Valentineorga@gmail.com>
2023-05-23 08:10:31 +0100

    export generated wallet to 'new-wallet.json' file. Optionally disable this with '--no-export' flag which will log the result instead

--- src/commands/generateAccount.ts
+++ src/commands/generateAccount.ts
@@ -1,29 +1,74 @@
 import { Args, Command, Flags } from "@oclif/core";
 import { ethers } from "ethers";
 import * as chalk from "chalk";
+import * as fs from "fs";
 
+interface NewWallet {
+  mnemonic?: string;
+  privateKey: string;
+  address: string;
+}
 export default class GenerateAccount extends Command {
   static description = "generates a new random account";
 
   static examples = ["<%= config.bin %> <%= command.id %> generateAccount"];
 
+  static flags = {
+    "no-export": Flags.boolean({
+      description:
+        "disables export for mnemonic and private key to 'new-wallet.json' file in current directory",
+      default: false,
+    }),
+  };
+
   public async run(): Promise<void> {
+    const { flags } = await this.parse(GenerateAccount);
+
     const wallet = ethers.Wallet.createRandom();
-    if (wallet.mnemonic) {
+
+    if (flags["no-export"]) {
+      if (wallet.mnemonic) {
+        console.log(
+          `MNEMONIC(${chalk.yellow.underline.bold(
+            "DO NOT REVEAL THIS TO ANYONE!!!"
+          )}):\n`,
+          wallet.mnemonic.phrase
+        );
+      }
+
       console.log(
-        `MNEMONIC(${chalk.red.underline.bold(
+        `\nPRIVATE KEY(${chalk.yellow.underline.bold(
           "DO NOT REVEAL THIS TO ANYONE!!!"
         )}):\n`,
-        wallet.mnemonic.phrase
+        wallet.privateKey
       );
-    }
 
-    console.log(
-      `\nPRIVATE KEY(${chalk.red.underline.bold(
-        "DO NOT REVEAL THIS TO ANYONE!!!"
-      )}):\n`,
-      wallet.privateKey
-    );
-    console.log("\nADDRESS:\n", wallet.address);
+      console.log("\nADDRESS:\n", wallet.address);
+    } else {
+      let newWallet: NewWallet = {
+        address: wallet.address,
+        privateKey: wallet.privateKey,
+      };
+
+      if (wallet.mnemonic) {
+        newWallet.mnemonic = wallet.mnemonic.phrase;
+      }
+
+      fs.writeFile("new-wallet.json", JSON.stringify(newWallet), (error) => {
+        if (error) {
+          throw new Error(JSON.stringify(error));
+        } else {
+          console.log("Wallet created successfully✅");
+          console.log(
+            `Written to ${chalk.green.underline.bold("./new-wallet.json")}`
+          );
+          console.log(
+            chalk.yellow.bold(
+              "DO NOT REVEAL YOUR MNEMONIC AND PRIVATE KEY TO ANYONE... PROTECT YOUR FUNDS!!!"
+            )
+          );
+        }
+      });
+    }
   }
 }
